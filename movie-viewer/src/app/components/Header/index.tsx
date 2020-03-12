a2034f2aa7b65da5dda5945219574343eb732f0a
dzmitry.bely <dzmitriibely@gmail.com>
2020-03-12 13:55:12 +0300

    add filter block

--- movie-viewer/src/app/components/Header/index.tsx
+++ movie-viewer/src/app/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import * as styles from './style.css';
 
 interface IProps {
@@ -6,15 +7,18 @@ interface IProps {
 }
 
 export const Header: React.FC<IProps> = ({ children = null }) => {
+  const logoWrapperStyles = classNames('container', styles.logoWrapper);
+  const contentWrapperStyles = classNames('container', styles.contentWrapper);
+
   return (
     <header className={styles.header}>
       <div className={styles.bg}></div>
-      <div className={styles.logoWrapper}>
+      <div className={logoWrapperStyles}>
         <div className={styles.logo}>
           netflix<span>roulette</span>
         </div>
       </div>
-      <div className={styles.contentWrapper}>{children}</div>
+      <div className={contentWrapperStyles}>{children}</div>
     </header>
   );
 };
--- movie-viewer/src/app/containers/Home/index.tsx
+++ movie-viewer/src/app/containers/Home/index.tsx
@@ -6,20 +6,36 @@ interface IProps {}
 
 export const Home: React.FC<IProps> = ({}) => {
   return (
-    <Header>
-      <div className={styles.inputWrapper}>
-        <Input id="text" type="text" />
-        <Button>Search</Button>
+    <>
+      <Header>
+        <div className={styles.inputWrapper}>
+          <Input id="text" type="text" />
+          <Button>Search</Button>
+        </div>
+        <div className={styles.btnGroup}>
+          <p>Search by:</p>
+          <ButtonGroup>
+            <Button active className={styles.btn}>
+              Title
+            </Button>
+            <Button className={styles.btn}>Genre</Button>
+          </ButtonGroup>
+        </div>
+      </Header>
+      <div className={styles.filterWrapper}>
+        <div className="container">
+          <p>7 movies found</p>
+          <div className={styles.btnGroup}>
+            <p>Sort by:</p>
+            <ButtonGroup>
+              <Button active className={styles.btn}>
+                Release date
+              </Button>
+              <Button className={styles.btn}>Rating</Button>
+            </ButtonGroup>
+          </div>
+        </div>
       </div>
-      <div className={styles.btnGroup}>
-        <p>Search by:</p>
-        <ButtonGroup>
-          <Button active className={styles.btn}>
-            Title
-          </Button>
-          <Button className={styles.btn}>Genre</Button>
-        </ButtonGroup>
-      </div>
-    </Header>
+    </>
   );
 };
